47c1552ee08525adf3db689315e30f97e2e47d02
Richard Lo <richardjosonlo@gmail.com>
2013-11-13 23:59:32 -0800

    added checkout page

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/subscriptions', routes.subscriptions);
 app.get('/checkout', routes.checkout);
 app.get('/users', user.list);
 
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Sumo Jerky' });
+  res.render('index', { title: 'Sumo Jerky - Monthly Jerky Delivery Service' });
+};
+
+exports.subscriptions = function(req, res){
+  res.render('subscriptions', { title: 'Sumo Jerky - Monthly Jerky Delivery Service' });
 };
 
 exports.checkout = function(req, res){
-  res.render('checkout', { title: 'Sumo Jerky' });
+  res.render('checkout', { title: 'Sumo Jerky - Monthly Jerky Delivery Service' });
 };
